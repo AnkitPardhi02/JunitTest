355ad91a883d3389ce9a2d7f07cd678112452151
Ankitpardhi02 <ankitpardhi1012@gmail.com>
2023-02-02 12:12:28 +0530

    Frist Commit in Junit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public int addNumber(int x,int y){
+        return x+y;
+    }
+    public int subNumber(int x,int y){
+        return x-y;
+
+    }
+    public int multiplyNumber(int x,int y){
+        return x*y;
+    }
+    public int divideNumber(int x,int y){
+        return x/y;
+    }
+    public int moduloNumber(int x,int y){
+        return x%y;
+    }
+
+
+
+}
